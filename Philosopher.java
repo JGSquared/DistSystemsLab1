533e2e816a5aa5e3e05ca93c1cd24e21efd19536
IanHavens <havensid@rose-hulman.edu>
2017-04-01 16:32:05 -0400

    removed warnings from philosopher

--- Philosopher.java
+++ Philosopher.java
@@ -1,16 +1,7 @@
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Random;
-import java.util.Timer;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
