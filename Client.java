fcc96c0d2f86f8ff1a9cab1f1ec444025b291559
IanHavens <havensid@rose-hulman.edu>
2017-04-23 15:58:29 -0400

    client logic change up

--- Client.java
+++ Client.java
@@ -15,7 +15,7 @@ class Client implements Runnable {
 		this.port = port;
 		this.ipAddresses = ipAddresses;
 		Philosopher.state = Philosopher.STATE.THINKING;
-//		Philosopher.state = Philosopher.STATE.SLEEPING;
+		// Philosopher.state = Philosopher.STATE.SLEEPING;
 
 		if (ipAddresses[2].equals("gui")) {
 			this.isRandom = false;
@@ -89,6 +89,9 @@ class Client implements Runnable {
 		long eatStart = 0;
 		long eatEnd = 0;
 
+		long drinkStart = 0;
+		long drinkEnd = 0;
+
 		boolean tooLongFlag = false;
 
 		while (true) {
@@ -114,6 +117,8 @@ class Client implements Runnable {
 				if (Philosopher.state == Philosopher.STATE.THINKING) {
 					eatStart = 0;
 					eatEnd = 0;
+					drinkStart = 0;
+					drinkEnd = 0;
 					tooLongFlag = false;
 					if (!this.isRandom) {
 						Philosopher.textArea.setText("THINKING");
@@ -254,17 +259,27 @@ class Client implements Runnable {
 					if (!this.isRandom) {
 						Philosopher.textArea.setText("THIRSTY");
 					}
-					
-					if (this.isRandom) {
-						
+					System.out.println("THIRSTY");
+					if (Philosopher.hadCupLast) {
+						Philosopher.state = Philosopher.STATE.DRINKING;
+					} else {
+						try {
+							Thread.sleep(1500);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
 					}
 				}
 
-				if (Philosopher.hadCupLast) {
-					System.out.println("I HAVE THE CUP");
-					boolean isThirsty = Math.random() > .5;
-					if (isThirsty) {
-						System.out.println("THIRSTY");
+				if (Philosopher.state == Philosopher.STATE.DRINKING) {
+					if (drinkStart == 0) {
+						drinkStart = System.currentTimeMillis();
+					}
+					if (!this.isRandom && !tooLongFlag) {
+						Philosopher.textArea.setText("DRINKING");
+					}
+
+					if (this.isRandom) {
 						int drinkTime = rand.nextInt(6000);
 						try {
 							System.out.println("DRINKING");
@@ -296,24 +311,75 @@ class Client implements Runnable {
 								e.printStackTrace();
 							}
 						}
-					} else {
-						try {
-							leftOut.write(2);
-							int response = leftIn.read();
-							if (response == 1) {
-								synchronized (Philosopher.cupLock) {
-									Philosopher.hadCupLast = false;
-									Philosopher.needToPass = false;
-								}
-							}
-						} catch (SocketTimeoutException e) {
-							continue;
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
+					}
+
+					drinkEnd = System.currentTimeMillis();
+
+					if (drinkEnd - drinkStart > 5000) {
+						Philosopher.textArea.setText("DRINKING TOO LONG");
+						tooLongFlag = true;
 					}
 				}
+
+//				if (Philosopher.hadCupLast) {
+//					System.out.println("I HAVE THE CUP");
+//					boolean isThirsty = Math.random() > .5;
+//					if (isThirsty) {
+//						System.out.println("THIRSTY");
+//						int drinkTime = rand.nextInt(6000);
+//						try {
+//							System.out.println("DRINKING");
+//							Thread.sleep(drinkTime);
+//						} catch (InterruptedException e) {
+//							e.printStackTrace();
+//						}
+//						Philosopher.needToPass = true;
+//						if (drinkTime > 5000) {
+//							synchronized (Philosopher.stateLock) {
+//								Philosopher.state = Philosopher.STATE.SLEEPING;
+//							}
+//						} else {
+//							try {
+//								leftOut.write(2);
+//								int response = leftIn.read();
+//								System.out.println("PASSED THE CUP");
+//								if (response == 1) {
+//									synchronized (Philosopher.cupLock) {
+//										Philosopher.hadCupLast = false;
+//										Philosopher.needToPass = false;
+//									}
+//								}
+//							} catch (SocketTimeoutException e) {
+//								if (Philosopher.state != Philosopher.STATE.SLEEPING) {
+//									continue;
+//								}
+//							} catch (IOException e) {
+//								e.printStackTrace();
+//							}
+//						}
+//					} else {
+//						try {
+//							leftOut.write(2);
+//							int response = leftIn.read();
+//							if (response == 1) {
+//								synchronized (Philosopher.cupLock) {
+//									Philosopher.hadCupLast = false;
+//									Philosopher.needToPass = false;
+//								}
+//							}
+//						} catch (SocketTimeoutException e) {
+//							continue;
+//						} catch (IOException e) {
+//							e.printStackTrace();
+//						}
+//					}
+//				}
 			} else {
+				synchronized (Philosopher.chopLock) {
+					Philosopher.haveLeftChopstick = false;
+					Philosopher.haveRightChopstick = false;
+				}
+				
 				try {
 					System.out.println("SLEEPING");
 					Thread.sleep(2000);
